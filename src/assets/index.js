1cacaca85a7cb3a767e6c386d68b4cf4de8152e7
7twik <sattwik.das.ece25@heritageit.edu.in>
2023-03-08 23:59:02 +0530

    1st commit

---
+++ src/assets/index.js
@@ -0,0 +1,78 @@
+import logo from "./logo.svg";
+import logo1 from "./logo1.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import iq from "./iq.jpeg";
+import digitalOcean from "./digitalOcean.png";
+import solidity from "./solidity.svg";
+import blockchain from "./blockchain.png";
+import vercel from "./vercel.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import trackersy from "./trackersy.png";
+import hawkershut from "./HawkersHut.png";
+import bitapods from "./47bitapods.png";
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  logo1,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  iq,
+  digitalOcean,
+  solidity,
+  blockchain,
+  vercel,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  trackersy,
+  bitapods,
+  hawkershut,
+  tripguide,
+};
